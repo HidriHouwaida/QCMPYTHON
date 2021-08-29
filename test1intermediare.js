cbcbcd6f48c43f71934dac88e63db76a21911e1d
HidriHouwaida <houwaidahidri@gmail.com>
2021-08-30 00:11:57 +0100

    initial

---
+++ test1intermediare.js
@@ -0,0 +1,4 @@
+function test1()
+{
+    window.open('test1question1intermediare.html','_self',false)
+}
---
+++ test1question3intermediare.js
@@ -0,0 +1,23 @@
+function button1()
+{
+  document.getElementById('button1').style.backgroundColor='red';
+  var audio=new Audio('song/faux.mp3') 
+  audio.play() 
+}
+function button2()
+{
+  document.getElementById('button2').style.backgroundColor='green';
+  window.open('test1question4intermediare.html','_self',false)
+}
+function button3()
+{
+  document.getElementById('button3').style.backgroundColor='red';
+  var audio=new Audio('song/faux.mp3') 
+  audio.play() 
+}
+function button4()
+{
+  document.getElementById('button4').style.backgroundColor='red';
+  var audio=new Audio('song/faux.mp3') 
+  audio.play() 
+}
