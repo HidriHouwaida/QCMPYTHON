a9adf6ee542b8a981cf3cf419f667b474c1058fe
HidriHouwaida <houwaidahidri@gmail.com>
2021-09-02 04:09:55 +0100

    initial

--- test1intermediare.js
+++ test1intermediare.js
@@ -6,3 +6,7 @@ function test2()
 {
     window.open('test2question1intermediare.html','_self',false)
 }
+function test3()
+{
+    window.open('test3question1intermediare.html','_self',false)
+}
---
+++ test3question4intermediare.js
@@ -0,0 +1,11 @@
+function button1()
+{
+  document.getElementById('button1').style.backgroundColor='green';
+  window.open('test3question5intermediare.html','_self',false)
+}
+function button2()
+{
+  document.getElementById('button2').style.backgroundColor='red';
+  var audio=new Audio('song/faux.mp3');
+  audio.play();
+}
